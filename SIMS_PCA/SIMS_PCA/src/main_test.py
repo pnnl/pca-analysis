f89ca1b31fdba4bf3632d367d9e3e57d555369f8
Welch, Cole S <cole.welch@pnnl.gov>
2023-09-19 16:38:51 -0700

    Text entry boxes and positive/negative ion selection button work now, and string paths / directories are updated appropriately.

--- SIMS_PCA/SIMS_PCA/src/main_test.py
+++ SIMS_PCA/SIMS_PCA/src/main_test.py
@@ -14,7 +14,7 @@ class App(customtkinter.CTk):
     def __init__(self):
         # --------------------------------------------------------------------------- Setup for basic GUI ---------------------------------------------------------------------------
         super().__init__()
-        self.geometry('1080x720')
+        self.geometry('960x720')
         customtkinter.set_appearance_mode('dark')
         customtkinter.set_default_color_theme('blue')
         self.title('PCA Analysis Hub')
@@ -23,28 +23,28 @@ class App(customtkinter.CTk):
 
         # TODO Figure out best way to add a callback command to get the entry values below (use a button?)
         # Add entries for the paths to essential files
-        self.pcaDir_label = customtkinter.CTkLabel(self, text='Enter PCA directory here: ', width=40, height=20)
-        self.pcaDir_label.grid(row=0, column=0, padx=50, pady=(50, 5), sticky="w")
-        self.pcaDir = customtkinter.CTkEntry(self, placeholder_text='/home/welch688/pca-analysis/SIMS_PCA/SIMS_PCA/', width=400, height=20)
-        self.pcaDir.grid(row=0, column=1, padx=50, pady=(50, 5), sticky="e")
+        self.pca_dir_label = customtkinter.CTkLabel(self, text='Enter PCA directory here: ', width=40, height=20)
+        self.pca_dir_label.grid(row=0, column=0, padx=10, pady=(50, 5), sticky="w")
+        self.pca_dir_entry = customtkinter.CTkEntry(self, textvariable=customtkinter.StringVar(value='/home/welch688/pca-analysis/SIMS_PCA/SIMS_PCA/'), width=400, height=20)
+        self.pca_dir_entry.grid(row=0, column=1, padx=10, pady=(50, 5))
 
         self.raw_data_label = customtkinter.CTkLabel(self, text='Enter name of raw data file with file extension here: ', width=40, height=20)
-        self.raw_data_label.grid(row=1, column=0, padx=50, pady=5, sticky="w")
-        self.raw_data = customtkinter.CTkEntry(self, placeholder_text='Hifh P Pasture_Chris_Positive.TXT', width=400, height=20)
-        self.raw_data.grid(row=1, column=1, padx=50, pady=5, sticky="e")
+        self.raw_data_label.grid(row=1, column=0, padx=10, pady=5, sticky="w")
+        self.raw_data_entry = customtkinter.CTkEntry(self, textvariable=customtkinter.StringVar(value='Hifh P Pasture_Chris_Positive.TXT'), width=400, height=20)
+        self.raw_data_entry.grid(row=1, column=1, padx=10, pady=5)
 
         self.report_label = customtkinter.CTkLabel(self, text='Enter desired name of report with file extension here: ', width=40, height=20)
-        self.report_label.grid(row=2, column=0, padx=50, pady=5, sticky="w")
-        self.report = customtkinter.CTkEntry(self, placeholder_text='report.docx', width=400, height=20)
-        self.report.grid(row=2, column=1, padx=50, pady=5, sticky="e")
+        self.report_label.grid(row=2, column=0, padx=10, pady=5, sticky="w")
+        self.report_entry = customtkinter.CTkEntry(self, textvariable=customtkinter.StringVar(value='report.docx'), width=400, height=20)
+        self.report_entry.grid(row=2, column=1, padx=10, pady=5)
 
 
         # TODO Confirm positive / negative choice callback works
         # Add buttons to select whether the report is for positive or negative ion data
-        self.label_pos_or_neg = customtkinter.CTkLabel(self, text='Select whether current report contains positive or negative ions: ', width=150, height=80)
-        self.label_pos_or_neg.grid(row=3, column=0, padx=50, pady=10, sticky='w')
+        self.label_pos_or_neg = customtkinter.CTkLabel(self, text='Select whether current data contains positive or negative ions: ', width=150, height=80)
+        self.label_pos_or_neg.grid(row=3, column=0, padx=10, pady=50, sticky='w')
         self.pos_or_neg = customtkinter.CTkSegmentedButton(self, width=100, height=80, command=self.pos_or_neg_button_callback)
-        self.pos_or_neg.grid(row=3, column=1, padx=50, pady=10, sticky='e')
+        self.pos_or_neg.grid(row=3, column=1, padx=10, pady=10, sticky='ew')
         self.pos_or_neg.configure(values=['positive', 'negative'])
         self.pos_or_neg.set('positive')
 
@@ -65,16 +65,57 @@ class App(customtkinter.CTk):
         # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
+        # ---------------------------------------------------- Initialize various paths and parameters for pcasims to use later------------------------------------------------------
+        # The main PCA folder (for example, if storing your data on Windows at PNNL, this could be
+        # /mnt/c/Users/<INSERT USERNAME>/'OneDrive - PNNL'/Documents/pca-analysis/SIMS_PCA/SIMS_PCA).
+        self.pca_dir = '/home/welch688/pca-analysis/SIMS_PCA/SIMS_PCA/'
+
+        # SIMS data
+        self.f_rawsims_data = os.path.join(self.pca_dir, 'sims-data/OriginalData/', 'Hifh P Pasture_Chris_Positive.TXT')
+
+        # Store the subset of groups from the data above which the user wants to analyze
+        self.f_group_numbers = os.path.join(self.pca_dir, 'sims-data/OriginalData/_groupnumbers.txt')
+
+        # SIMS metadata
+        self.f_metadata = os.path.join(self.pca_dir, 'sims-data/OriginalData/_metadata.txt')
+
+        # SIMS-PCA report
+        self.f_report = os.path.join(self.pca_dir, 'output_sample/', 'report.docx')
+
+        # Output folder
+        self.outDir = os.path.join(self.pca_dir, 'output_sample')
+
+        # Document positive and negative mass
+        self.f_doc_positive_mass = os.path.join(self.pca_dir, "sims-data", "positive_doc_mass_record.csv")
+        self.f_doc_negative_mass = os.path.join(self.pca_dir, "sims-data", "negative_doc_mass_record.csv")
+
+        # Indicates to rest of code whether we are handling positive or negative ions
+        self.positive_or_negative_ion = 'positive'
+        self.f_doc_mass = self.f_doc_positive_mass
+        # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
     # ------------------------------------------------------- Create the callback commands that will run the program ------------------------------------------------------------
-    #      Why aren't updates happening automatically?
-    # User input specifies which document of stored masses should be used for the pca simulations
     def pos_or_neg_button_callback(self, value: str):
         self.positive_or_negative_ion = value
+        print('Selected \'' + value + '\' doc mass.')
+
+        if self.positive_or_negative_ion == 'positive':
+            self.f_doc_mass = self.f_doc_positive_mass
+        elif self.positive_or_negative_ion == 'negative':
+            self.f_doc_mass = self.f_doc_negative_mass
+        else:
+            print('***Error! Invalid input for positive_or_negative_ion; choose \'positive\' or \'negative\'***')
+            sys.exit()
         
+
     # Update the report from user changes
     def update_callback(self):
         # Initialize pcasims if it doesn't already exist and update it with any user changes the user has made to its desired parameters
-        self.update_variables()
+        try:
+            self.update_pca_instance()
+        except:
+            return
 
         print('-------->Updating Peak Assignments from User Changes to Report...')
 
@@ -85,7 +126,11 @@ class App(customtkinter.CTk):
 
     # Generate the report
     def generate_callback(self):
-        self.update_variables()
+        # Initialize pcasims if it doesn't already exist and update it with any user changes the user has made to its desired parameters
+        try:
+            self.update_pca_instance()
+        except:
+            return
 
         # Perform PCA
         self.pcasims.perform_pca()
@@ -107,48 +152,37 @@ class App(customtkinter.CTk):
     # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     # ------------------------------------------------------------- Set up backend input based on user changes  -----------------------------------------------------------------
-    def update_variables(self):
-        # The main PCA folder (for example, if storing your data on Windows at PNNL, this could be
-        # /mnt/c/Users/<INSERT USERNAME>/'OneDrive - PNNL'/Documents/pca-analysis/SIMS_PCA/SIMS_PCA).
-        self.pcaDir = '/home/welch688/pca-analysis/SIMS_PCA/SIMS_PCA/'
-
-        # Output folder
-        self.outDir = os.path.join(self.pcaDir, 'output_sample')
-
-        # Indicates to rest of code whether we are handling positive or negative ions
-        self.positive_or_negative_ion = 'positive'
-
-        # SIMS data
-        self.f_rawsims_data = os.path.join(self.pcaDir, 'sims-data/OriginalData/Hifh P Pasture_Chris_Positive.TXT')
-
-        # Store the subset of groups from the data above which the user wants to analyze
-        self.f_group_numbers = os.path.join(self.pcaDir, 'sims-data/OriginalData/_groupnumbers.txt')
-
-        # SIMS metadata
-        self.f_metadata = os.path.join(self.pcaDir, 'sims-data/OriginalData/_metadata.txt')
-
-        # SIMS-PCA report
-        self.f_report = os.path.join(self.pcaDir, 'output_sample/report.docx')
-
-
-        # Document positive and negative mass
-        self.f_doc_positive_mass = os.path.join(self.pcaDir, "sims-data", "positive_doc_mass_record.csv")
-        self.f_doc_negative_mass = os.path.join(self.pcaDir, "sims-data", "negative_doc_mass_record.csv")
-
-        # TODO How to make this change pcasims properly? Need to update this before calling pca_sims(), but mainloop occurs later?
-        # User input specifies which document of stored masses should be used for the pca simulations
-        if self.positive_or_negative_ion == 'positive':
-            self.f_doc_mass = self.f_doc_positive_mass
-        elif self.positive_or_negative_ion == 'negative':
-            self.f_doc_mass = self.f_doc_negative_mass
+    def update_pca_instance(self):
+        # Update PCA directory
+        if (('pca-analysis/SIMS_PCA/SIMS_PCA/' in self.pca_dir_entry.get()) and self.pca_dir_entry.get()[-1] == '/'):
+            self.pca_dir = self.pca_dir_entry.get()
+            print('-------->Processed PCA directory successfully.')
+        elif (not self.pca_dir_entry.get()):
+            print('***Error! Empty entry. Please enter text and try again.***')
+            raise ValueError
         else:
-            print('***Error! Invalid input for positive_or_negative_ion; choose \'positive\' or \'negative\'***')
-            sys.exit()
+            print('***Error! Invalid input for pca_dir. Please make sure it ends with \'pca-analysis/SIMS_PCA/SIMS_PCA/\' and try again.***')
+            raise ValueError
+        
+        # Update raw data file
+        if (self.raw_data_entry.get()):
+            self.f_rawsims_data = os.path.join(self.pca_dir, 'sims-data/OriginalData/', self.raw_data_entry.get())
+            print('-------->Processed raw data file name successfully.')
+        else:
+            print('***Error! Empty entry. Please enter text and try again.***')
+            raise ValueError
+
+        # Update report
+        if (self.report_entry.get()):
+            self.f_report = os.path.join(self.pca_dir, 'output_sample/', self.report_entry.get())
+            print('-------->Processed report file name successfully.')
+        else:
+            print('***Error! Empty entry. Please enter text and try again.***')
+            raise ValueError
 
         # Initialize the pca_sims instance
-        self.pcasims = pca_sims(self.f_rawsims_data, self.f_metadata, self.f_doc_mass, self.pcaDir, self.outDir, self.positive_or_negative_ion, self.f_group_numbers)
+        self.pcasims = pca_sims(self.f_rawsims_data, self.f_metadata, self.f_doc_mass, self.pca_dir, self.outDir, self.positive_or_negative_ion, self.f_group_numbers)
     # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 
 
 

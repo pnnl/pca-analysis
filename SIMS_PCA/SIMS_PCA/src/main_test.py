3b7b8c3176cf199c3093c9dea617b8fc7d7e82cb
Welch, Cole S <cole.welch@pnnl.gov>
2024-02-12 15:24:51 -0800

    Added new window and basic treeview blockout for the new catalog interface.

--- SIMS_PCA/SIMS_PCA/src/main_test.py
+++ SIMS_PCA/SIMS_PCA/src/main_test.py
@@ -1,8 +1,10 @@
-import customtkinter
+import customtkinter as ctk
+from tkinter import ttk
 import os
 import sys
 import subprocess
 import logging
+import pandas as pd
 # Disable matplotlib font logging (it outputs unnecessary info about missing fonts)
 logging.getLogger('matplotlib.font_manager').setLevel(logging.ERROR)
 
@@ -11,69 +13,64 @@ from pca_sims.pca_sims import pca_sims
 
 # TODO Fix redundant initialization of paths and pos_or_neg between Ctk entries / buttons and original code
 # Specifies the basic structure of the GUI application
-class App(customtkinter.CTk):
+class App(ctk.CTk):
     def __init__(self):
-        # --------------------------------------------------------------------------- Setup for basic GUI ---------------------------------------------------------------------------
+        # --------------------------------------------------------------------------- Setup for basic GUI -------------------------------------------------------------------
         super().__init__()
         self.geometry('960x720')
-        customtkinter.set_appearance_mode('dark')
-        customtkinter.set_default_color_theme('blue')
+        ctk.set_appearance_mode('dark')
+        ctk.set_default_color_theme('blue')
         self.title('PCA Analysis Hub')
         self.grid_columnconfigure(0, weight=1)
 
 
         # Add entries for the paths to essential files
-        self.pca_dir_label = customtkinter.CTkLabel(self, text='Enter PCA directory here: ', width=40, height=20)
+        self.pca_dir_label = ctk.CTkLabel(self, text='Enter PCA directory here: ', width=40, height=20)
         self.pca_dir_label.grid(row=0, column=0, padx=10, pady=(50, 5), sticky="w")
-        self.pca_dir_entry = customtkinter.CTkEntry(self, textvariable=customtkinter.StringVar(value='/home/welch688/pca-analysis/SIMS_PCA/SIMS_PCA/'), width=400, height=20)
+        self.pca_dir_entry = ctk.CTkEntry(self, textvariable=ctk.StringVar(value='/home/welch688/pca-analysis/SIMS_PCA/SIMS_PCA/'), width=400, height=20)
         self.pca_dir_entry.grid(row=0, column=1, padx=10, pady=(50, 5))
 
-        self.raw_data_label = customtkinter.CTkLabel(self, text='Enter name of raw data file with file extension here: ', width=40, height=20)
+        self.raw_data_label = ctk.CTkLabel(self, text='Enter name of raw data file with file extension here: ', width=40, height=20)
         self.raw_data_label.grid(row=1, column=0, padx=10, pady=5, sticky="w")
-        self.raw_data_entry = customtkinter.CTkEntry(self, textvariable=customtkinter.StringVar(value='High P Pasture_Chris_Positive.txt'), width=400, height=20)
+        self.raw_data_entry = ctk.CTkEntry(self, textvariable=ctk.StringVar(value='High P Pasture_Chris_Positive.txt'), width=400, height=20)
         self.raw_data_entry.grid(row=1, column=1, padx=10, pady=5)
 
-        self.report_label = customtkinter.CTkLabel(self, text='Enter desired name of report with file extension here: ', width=40, height=20)
+        self.report_label = ctk.CTkLabel(self, text='Enter desired name of report with file extension here: ', width=40, height=20)
         self.report_label.grid(row=2, column=0, padx=10, pady=5, sticky="w")
-        self.report_entry = customtkinter.CTkEntry(self, textvariable=customtkinter.StringVar(value='Report High P Pasture_Chris_Positive.docx'), width=400, height=20)
+        self.report_entry = ctk.CTkEntry(self, textvariable=ctk.StringVar(value='Report High P Pasture_Chris_Positive.docx'), width=400, height=20)
         self.report_entry.grid(row=2, column=1, padx=10, pady=5)
 
 
         # Add buttons to select whether the report is for positive or negative ion data
-        self.label_pos_or_neg = customtkinter.CTkLabel(self, text='Select whether current data contains positive or negative ions: ', width=15, height=80)
+        self.label_pos_or_neg = ctk.CTkLabel(self, text='Select whether current data contains positive or negative ions: ', width=15, height=80)
         self.label_pos_or_neg.grid(row=3, column=0, padx=10, pady=10, sticky='w')
-        self.pos_or_neg = customtkinter.CTkSegmentedButton(self, width=400, height=80, command=self.pos_or_neg_button_callback)
+        self.pos_or_neg = ctk.CTkSegmentedButton(self, width=400, height=80, command=self.pos_or_neg_button_callback)
         self.pos_or_neg.grid(row=3, column=1, padx=10, pady=10)
         self.pos_or_neg.configure(values=['positive', 'negative'])
         self.pos_or_neg.set('positive')
 
 
-        # Add checkboxes for selecting sample group numbers
-        self.button_sample_groups = customtkinter.CTkButton(self, text='Edit group numbers to analyze', width=400, height=40, command=self.sample_group_callback)
-        self.button_sample_groups.grid(row=4, column=1, padx=50, pady=5)
-
-
-        # Add button to pull up metadata file in order to edit it
-        self.button_metadata = customtkinter.CTkButton(self, text='Edit metadata file', width=400, height=40, command=self.metadata_callback)
-        self.button_metadata.grid(row=5, column=1, padx=50, pady=5)
+        # Add button to edit the catalog, through which users can select sample numbers and edit metadata.
+        self.button_catalog = ctk.CTkButton(self, text='Edit catalog', width=400, height=40, command=self.catalog_callback)
+        self.button_catalog.grid(row=4, column=1, padx=50, pady=5)
 
 
         # Add entry to change number of PCA components desired for plots and tables in the report
-        self.pcacomp_label = customtkinter.CTkLabel(self, text='Change number of PCA components used in report:', width=40, height=20)
+        self.pcacomp_label = ctk.CTkLabel(self, text='Change number of PCA components used in report:', width=40, height=20)
         self.pcacomp_label.grid(row=6, column=0, padx=10, pady=(50,10), sticky='w')
-        self.pcacomp_entry = customtkinter.CTkEntry(self, textvariable=customtkinter.StringVar(value='5'), width=400, height=20)
+        self.pcacomp_entry = ctk.CTkEntry(self, textvariable=ctk.StringVar(value='5'), width=400, height=20)
         self.pcacomp_entry.grid(row=6, column=1, padx=10, pady=(50,10))
 
 
         # Finally, add a button to update the document values from the report and another to generate the report from the current database
-        self.button_update = customtkinter.CTkButton(self, text='Update document values database from report', width=980, height=50, command=self.update_callback)
+        self.button_update = ctk.CTkButton(self, text='Update document values database from report', width=980, height=50, command=self.update_callback)
         self.button_update.grid(row=7, padx=50, pady=(50, 5), sticky='ew', columnspan=2)
-        self.button_generate = customtkinter.CTkButton(self, text='Generate PCA report from current database', width=980, height=50, command=self.generate_callback)
+        self.button_generate = ctk.CTkButton(self, text='Generate PCA report from current catalog and database', width=980, height=50, command=self.generate_callback)
         self.button_generate.grid(row=8, padx=50, pady=(5, 50), sticky='ew', columnspan=2)
-        # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        # -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-        # ---------------------------------------------------- Initialize various paths and parameters for pcasims to use later------------------------------------------------------
+        # ---------------------------------------------------- Initialize various paths and parameters for pcasims to use later----------------------------------------------
         # The main PCA folder (for example, if storing your data on Windows at PNNL, this could be
         # /mnt/c/Users/<INSERT USERNAME>/'OneDrive - PNNL'/Documents/pca-analysis/SIMS_PCA/SIMS_PCA).
         self.pca_dir = '/home/welch688/pca-analysis/SIMS_PCA/SIMS_PCA/'
@@ -82,9 +79,11 @@ class App(customtkinter.CTk):
         # SIMS data
         self.f_rawsims_data = os.path.join(self.pca_dir, 'sims-data/OriginalData/', 'High P Pasture_Chris_Positive.txt')
 
+        # TODO Transition --> catalog
         # Store the subset of groups from the data above which the user wants to analyze
         self.f_group_numbers = os.path.join(self.pca_dir, 'sims-data/OriginalData/_groupnumbers.txt')
 
+        # TODO Transition --> catalog
         # SIMS metadata
         self.f_metadata = os.path.join(self.pca_dir, 'sims-data/OriginalData/_metadata.txt')
 
@@ -101,10 +100,10 @@ class App(customtkinter.CTk):
         # Indicates to rest of code whether we are handling positive or negative ions
         self.positive_or_negative_ion = 'positive'
         self.f_doc_mass = self.f_doc_positive_mass if self.positive_or_negative_ion == 'positive' else self.f_doc_negative_mass
-        # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        # -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-    # ------------------------------------------------------- Create the callback commands that will run the program ------------------------------------------------------------
+    # ------------------------------------------------------- Create the callback commands that will run the program --------------------------------------------------------
     # Callback on positive / negative button that identifies the type of ions in the data file
     def pos_or_neg_button_callback(self, value: str):
         self.positive_or_negative_ion = value
@@ -119,14 +118,18 @@ class App(customtkinter.CTk):
             sys.exit()
 
 
-    # TODO Change from directly editing file to scrollable checkbox dropdown contained in UI
-    # Callback on button_sample_groups that opens the sample groups file for the user to edit
-    def sample_group_callback(self):
+    # TODO Finish implementing catalog details
+    # Callback on button_catalog that opens a checklist of sample groups, metadata, and more for users to customize their PCA report output.
+    def catalog_callback(self):
         # Update PCA directory
         if (('SIMS_PCA/SIMS_PCA/' in self.pca_dir_entry.get()) and self.pca_dir_entry.get()[-1] == '/'):
             self.pca_dir = self.pca_dir_entry.get()
-            self.f_group_numbers = os.path.join(self.pca_dir, 'sims-data/OriginalData/_groupnumbers.txt')
             print('-------->Processed PCA directory successfully.')
+
+            # Get the catalog file contents for later use
+            self.catalog = pd.read_csv(os.path.join(self.pca_dir, 'sims-data/Catalog/catalog.csv'))
+            # Create a CTk window for the catalog
+            self.catalog_window = CatalogWindow(self.catalog)
         elif (not self.pca_dir_entry.get()):
             print('***Error! Empty entry. Please enter text and try again.***')
             raise ValueError
@@ -134,25 +137,8 @@ class App(customtkinter.CTk):
             print('***Error! Invalid input for pca_dir. Please make sure it ends with \'pca-analysis/SIMS_PCA/SIMS_PCA/\' and try again.***')
             raise ValueError
         
-        subprocess.call(['notepad.exe', self.f_group_numbers])
-
-
-    # TODO Change from directly editing file to CTkEntries contained in UI
-    # Callback on button_metadata that opens the metadata file for the user to edit
-    def metadata_callback(self):
-        # Update PCA directory
-        if (('SIMS_PCA/SIMS_PCA/' in self.pca_dir_entry.get()) and self.pca_dir_entry.get()[-1] == '/'):
-            self.pca_dir = self.pca_dir_entry.get()
-            self.f_metadata = os.path.join(self.pca_dir, 'sims-data/OriginalData/_metadata.txt')
-            print('-------->Processed PCA directory successfully.')
-        elif (not self.pca_dir_entry.get()):
-            print('***Error! Empty entry. Please enter text and try again.***')
-            raise ValueError
-        else:
-            print('***Error! Invalid input for pca_dir. Please make sure it ends with \'pca-analysis/SIMS_PCA/SIMS_PCA/\' and try again.***')
-            raise ValueError
-        
-        subprocess.call(['notepad.exe', self.f_metadata])
+        # TODO Remove
+        # subprocess.call(['notepad.exe', self.f_group_numbers])
 
 
     # Update the report from user changes
@@ -196,14 +182,15 @@ class App(customtkinter.CTk):
         print('-------->Data Exporting...')
         print('\n\n\n-------->Congratulations!')
         print('-------->Please Check Results in the output_sample Folder.')
-    # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-    # ------------------------------------------------------------- Set up backend input based on user changes  -----------------------------------------------------------------
+    # ------------------------------------------------------------- Set up backend input based on user changes  -------------------------------------------------------------
     def update_pca_instance(self):
         # Update PCA directory along with group numbers and metadata
         if (('SIMS_PCA/SIMS_PCA/' in self.pca_dir_entry.get()) and self.pca_dir_entry.get()[-1] == '/'):
             self.pca_dir = self.pca_dir_entry.get()
+            # TODO Transition --> catalog
             self.f_group_numbers = os.path.join(self.pca_dir, 'sims-data/OriginalData/_groupnumbers.txt')
             self.f_metadata = os.path.join(self.pca_dir, 'sims-data/OriginalData/_metadata.txt')
             print('-------->Processed PCA directory successfully.')
@@ -243,9 +230,10 @@ class App(customtkinter.CTk):
             print('***Error! Empty entry. Please enter text and try again.***')
             raise ValueError
 
+        # TODO Transition --> catalog
         # Initialize the pca_sims instance
         self.pcasims = pca_sims(self.f_rawsims_data, self.f_metadata, self.f_doc_mass, self.pca_dir, self.outDir, self.positive_or_negative_ion, self.f_group_numbers)
-    # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     # Useful helper method for telling whether value x can be cast to int
     def isint(self, x):
@@ -257,7 +245,63 @@ class App(customtkinter.CTk):
         return True
 
 
+# Handles a second pop-up window for the catalog
+class CatalogWindow(ctk.CTkToplevel):
+    def __init__(self, catalog_df:pd.DataFrame):
+        # ------------------------------------------------------------------------ Setup for catalog GUI --------------------------------------------------------------------
+        super().__init__()
+        self.geometry('1920x720')
+        self.title('Catalog')
+        self.grid_columnconfigure(0, weight=1)
 
+        # Save the catalog DataFrame
+        self.catalog = catalog_df
+
+        # Add treeview (table) to display catalog information
+        treeview = ttk.Treeview(self, height=6)
+
+        # Get columns from DataFrame
+        treeview['columns'] = tuple(self.catalog.columns)
+        # Remove the 0th column since we don't need parent / child relationships.
+        treeview.column('#0', width=0, stretch='no')
+        # Add all the columns and headers to the treeview
+        for column in treeview['columns']:
+            treeview.column(column, anchor='center', minwidth=10, stretch=True)
+            treeview.heading(column, anchor='center', text=column)
+        
+        # Add all DataFrame entries to treeview
+        for index, row in self.catalog.iterrows():
+            treeview.insert(parent='', index='end', iid=index, values=list(row.values))
+
+        # Locate the treeview / table on the window
+        treeview.grid(row=0, padx=20, pady=20, sticky='ns', columnspan=1)
+
+
+        # Add button to save and exit
+        self.button_save = ctk.CTkButton(self, text='Save and exit', width=980, height=50, command=self.save_callback)
+        self.button_save.grid(row=1, padx=50, pady=(50, 5), sticky='ew', columnspan=1)
+
+
+        # Set ttk style and theme colors for this window
+        bg_color = self._apply_appearance_mode(ctk.ThemeManager.theme["CTkFrame"]["fg_color"])
+        text_color = self._apply_appearance_mode(ctk.ThemeManager.theme["CTkLabel"]["text_color"])
+        selected_color = self._apply_appearance_mode(ctk.ThemeManager.theme["CTkButton"]["fg_color"])
+
+        treestyle = ttk.Style()
+        treestyle.theme_use('default')
+        treestyle.configure("Treeview", background=bg_color, foreground=text_color, fieldbackground=bg_color, borderwidth=0)
+        treestyle.map('Treeview', background=[('selected', bg_color)], foreground=[('selected', selected_color)])
+        self.bind("<<TreeviewSelect>>", lambda event: self.focus_set())
+        # -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    # Saves current catalog and exits window
+    def save_callback(self):
+        # TODO Finish
+        # Saves the catalog file(s) in the default folder location
+
+        # TODO CLOSES EVERYTHING
+        # Close the window
+        self.quit()
 
 
 # --------------------------------------------------------------------------- Run the application ---------------------------------------------------------------------------
